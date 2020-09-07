53ffb7bbe1df7b146906b949468dbd36070e7288
Mayura Wijewickrama <mayura@ceylonit.com>
2020-09-07 10:06:27 +0530

    images

--- forms/contact.php
+++ forms/contact.php
@@ -7,35 +7,58 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // $receiving_email_address = 'mayurawijawickrama@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  // $contact->smtp = array(
+  //   'host' => 'smtp-relay.sendinblue.com',
+  //   'username' => 'mayurawijawickrama@gmail.com',
+  //   'password' => '4yHVLvNIBmP7Uskd',
+  //   'port' => '587'
+  // );
+  
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  // echo $contact->send();
+
+
+
+
+  include 'Mailin.php';
+  $mailin = new Mailin('mayurawijawickrama@gmail.com', '4yHVLvNIBmP7Uskd');
+  $mailin->
+  addTo('mayurawijawickrama@gmail.com', 'Nomad Vlogs')->
+  setFrom('mayurawijawickrama@gmail.com', 'Nomad Vlogs')->
+  setReplyTo('mayurawijawickrama@gmail.com','Nomad Vlogs')->
+  setSubject('Enter the subject here')->
+  setText('Hello')->
+  setHtml('<strong>Hello</strong>');
+  $res = $mailin->send();
+ 
+
+
+
+
+
+
+
+
 ?>
